5a8933027f4fdc4ab7349281b254289d3d9aa875
Baraja Swargiary <barajaswargiary4444@gmail.com>
2020-06-13 08:01:08 +0530

    Updated app.js

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.set('view engine', 'ejs')
 app.set('views', 'views')
 
+// render main index file
+
 app.get('/', (req, res) => {
 	res.render('index')
 })
